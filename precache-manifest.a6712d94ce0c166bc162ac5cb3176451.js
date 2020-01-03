547d59b9b369281bdce616416145ed554e4530ec
amazingratStudios <bar@eliezer.org>
2020-01-03 07:57:01 +0200

    Updates

---
+++ precache-manifest.a6712d94ce0c166bc162ac5cb3176451.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ce2f59782ddaf2dd8b3a1d438e279c5",
+    "url": "/ababurger/index.html"
+  },
+  {
+    "revision": "dce6cc2884d45a4ec6cb",
+    "url": "/ababurger/static/css/main.46ca4ce2.chunk.css"
+  },
+  {
+    "revision": "4e0b1471683803bdc30b",
+    "url": "/ababurger/static/js/2.8a7117f9.chunk.js"
+  },
+  {
+    "revision": "50cbc62d44650d1cd5fb5fba3b2ee67d",
+    "url": "/ababurger/static/js/2.8a7117f9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dce6cc2884d45a4ec6cb",
+    "url": "/ababurger/static/js/main.a9959816.chunk.js"
+  },
+  {
+    "revision": "fdeaa08c36ae0c26123d",
+    "url": "/ababurger/static/js/runtime-main.85266312.js"
+  }
+]);
